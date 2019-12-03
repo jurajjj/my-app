dfc2ad588d2b7d6fc45bead726a41594c110528f
Jiří Bayer <jiribayer@email.cz>
2019-12-03 16:25:38 +0100

    Updates

---
+++ precache-manifest.7e93a27449f543dc5b0d8cb1cf68227f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f03a184a8208b12ab86774403f23bf37",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "d28b56a9b9c45f402199",
+    "url": "/my-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8072c64069df12c48bcc",
+    "url": "/my-app/static/js/2.95e7e4f0.chunk.js"
+  },
+  {
+    "revision": "d28b56a9b9c45f402199",
+    "url": "/my-app/static/js/main.dda89347.chunk.js"
+  },
+  {
+    "revision": "2067abcc1b1d9bcd4e07",
+    "url": "/my-app/static/js/runtime-main.98b5c931.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my-app/static/media/logo.25bf045c.svg"
+  }
+]);
